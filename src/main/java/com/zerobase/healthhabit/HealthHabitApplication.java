381939e743d2875b76c901de4b4ec61f049b54a0
Delcastin <lejesu96@naver.com>
2025-05-26 00:49:41 +0900

    1st Commit & push

---
+++ src/main/java/com/zerobase/healthhabit/HealthHabitApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.healthhabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHabitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthHabitApplication.class, args);
+    }
+
+}
